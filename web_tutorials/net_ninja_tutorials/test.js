8aff0aca0b58cfbdaff73b3809ea6a9d475f7100
Dasaed <carlos.dasaed@gmail.com>
2018-11-09 10:18:48 -0330

    more notes

--- web_tutorials/net_ninja_tutorials/test.js
+++ web_tutorials/net_ninja_tutorials/test.js
@@ -1,1 +1,9 @@
 alert("Hello World")
+/**
+Little rules for JavaScript
+
+1) JavaScript is case sensitive
+2) every statement ends with a semicolon, and a statement is basically an action you want JavaScript to do (ie alert message, change background, etc. )
+
+
+*/ 
