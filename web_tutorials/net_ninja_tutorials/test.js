8076b70632818be452a296db28addcb9c026b408
Dasaed <carlos.dasaed@gmail.com>
2018-11-09 11:22:50 -0330

    side notes

--- web_tutorials/net_ninja_tutorials/test.js
+++ web_tutorials/net_ninja_tutorials/test.js
@@ -1,9 +1,14 @@
-alert("Hello World")
+document.write("Hello World written in the test.js JavaScript file")
+
 /**
 Little rules for JavaScript
-
 1) JavaScript is case sensitive
 2) every statement ends with a semicolon, and a statement is basically an action you want JavaScript to do (ie alert message, change background, etc. )
+3) White space doesn't matter
+4) Comments are done in the same way as in Java
+5) JavaScript runs from top to bottom
+**/
 
-
-*/ 
+var myVariableCarlos;
+myVariableCarlos=10;
+console.log("myVariableCarlos is equal to: "+myVariableCarlos+". \nJavaScript treats variables similar to python. In other words, you don't have to declare the type of variable. Everything is expressed with just the keyword 'var'");
