513edd4b4691dc3eb06dfb8ecfce579a1dfa598a
Dasaed <carlos.dasaed@gmail.com>
2018-11-14 20:45:47 -0330

    tutorial 19

--- web_tutorials/net_ninja_tutorials/test.js
+++ web_tutorials/net_ninja_tutorials/test.js
@@ -18,18 +18,48 @@ myVariableCarlos="My names is Carlos"
 console.log("Variables can easily change from one type to another and viceversa, depending on how you use them in your code.")
 
 //////////lesson 11 - control flow and if statements ////////////
-var myPI = function(myPI){
-  var temp = 0;
-    while(temp < myPI){
-
-    }           // The function returns the product of p1 and p2
-}
-}
-
+var myPI = 3.1;
 if (myPI > 3.14) {
   console.log("MyPI is greater than the actual value of pi. MyPI = "+myPI)
 } else if (myPI < 3.14){
   console.log("MyPI is less than the actual value of pi. MyPI = "+myPI)
 } else {
   console.log("MyPI is close enough to the actual value of pi. MyPI = "+myPI)
+}
+/////////////////////////////////////////////////////////////////
+
+var age = 5;
+while(age <=10 ){
+  console.log("Your age is less than 10");
+  age++;
+}
+document.write("you are now over 10");
+
+//////////////////////////////////////////////////////////////////
+var links = document.getElementsByTagName("a");
+for (i = 0; i <= links.length; i++){
+  console.log("this is link number "+i+" "+links[i]);
+}
+
+//////////////////////////////////////////////////////////////////
+// the "break" and "continue" keywords
+for (j = 0; j < 10; j++){
+  if (j === 5){
+    console.log("We are skipping 5");
+    continue;
+  }
+  console.log(i);
+
+  if (j === 7){
+    break;
+  }
+}
+console.log("I've broken out of the loop");
+
+//////////////////////////////////////////////////////////////////
+// example of a useful use for loops
+
+var links = document.getElementsByTagName("a");
+for (i = 0; i<  links.length; i++){
+  links[i].className = "Link-"+i; // we added an id to all links in an ordered list
 }
